208f52ac208f3d4ecb101d47483e994982d7cfc3
zongavids <anthonymacias11@gmail.com>
2016-01-16 09:38:50 -0800

    added Product Services

---
+++ src/microM/MicroM/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MicroM.Services;
+
+namespace MicroM.Controllers
+{
+    public class ProductsController : _MicroController
+    {
+
+        public ProductsController()
+        {
+            _service = new ProductService(_db);
+        }
+
+        private ProductService _service;
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult _List()
+        {
+            
+            return PartialView();
+        }
+    }
+}
