73a5fecbb5ee418127913a7fb1fa424103448d4e
Robert Sweeney <robert.sweeney@outlook.com>
2016-01-16 05:24:54 -0800

    adding bin service

---
+++ src/microM/MicroM/Services/BinService.cs
@@ -0,0 +1,16 @@
+﻿using MicroManage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroM.Services
+{
+    public class BinService
+    {
+        public MicroManageContext Context { get; private set; }
+        public BinService(MicroManageContext context) {
+            this.Context = context;
+        }
+    }
+}
