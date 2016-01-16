1fd84c3b11b127c8f4a52f89a35d992ebb9fc59e
Robert Sweeney <robert.sweeney@outlook.com>
2016-01-16 06:09:44 -0800

    adding strong type to post

---
+++ src/microM/MicroM/Models/InventoryUpdateMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace MicroM.Models
+{
+    public class InventoryUpdateMessage
+    {
+        public int ProductId { get; set; }
+        public string SerialId { get; set; }
+    }
+}
